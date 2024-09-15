0a55abdf6f73cd11eb24a27dc7c3de84ce4f3214
jozzydgr8 <jozzydgreat1@gmail.com>
2024-09-15 10:17:01 -0700

    added room grid

---
+++ src/Components/RoomGrid.jsx
@@ -0,0 +1,40 @@
+export const RoomGrid = ()=>{
+    return(
+        <div className="  row">
+            <div className="r-grid col-md-4">
+                <div>
+                    <div className="two">
+
+                    </div>
+                    <main>
+                        <h1>
+                            
+                        </h1>
+                    </main>
+                </div>
+            </div>
+            <div className="r-grid col-md-4">
+                <div>
+                    
+                        <div className="one">
+
+                        </div>
+                        <main>
+
+                        </main>
+                    
+                </div>
+            </div>
+            <div className="r-grid col-md-4">
+                <div>
+                <div className="three">
+
+                </div>
+                <main>
+
+                </main>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Pages/Rooms.jsx
+++ src/Pages/Rooms.jsx
@@ -1,8 +1,11 @@
+import { RoomGrid } from "../Components/RoomGrid"
+
 export const Rooms = ()=>{
     return(
         <section className="room">
             <div className="container-fluid">
                 <h1 className="heading" style={{textAlign:'center'}}>Our Rooms</h1>
+                <RoomGrid/>
             </div>
         </section>
     )
