fa336762abebb52de1953067c687068c6c1d1f1b
jozzydgr8 <jozzydgreat1@gmail.com>
2024-09-15 06:46:52 -0700

    added essentials'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './Home';
@@ -6,7 +5,7 @@ import Home from './Home';
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <>
+       <>
         <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>} />
         </Route>
--- src/Pages/Essentials.jsx
+++ src/Pages/Essentials.jsx
@@ -6,25 +6,71 @@ export const Essentials = ()=>{
                 <>
                 <div className="row">
                     
-                    <div className="col-md-6">
-                        <div className="e-flex">
-
+                    <div className="col-md-6 e-grid">
+                        <div>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <img src='https://cdn-icons-png.flaticon.com/512/90/90675.png'alt="lounge"/>
+                            </div>
+                            <div className="col-md-10">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                 Aliquam nostrum aliquid quas ipsam molestiae quidem cupiditate natus ullam odit nisi!
+                            <div className="e-div">
+                                unlimited wifi
+                            </div>
+                            </div>
+                        </div>
                         </div>
                     </div>
-                    <div className="col-md-6">
-                        <div className="e-flex">
-
+                    
+                    <div className="col-md-6 e-grid">
+                        <div>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy1VqMGxOYQnYMNRms65wJ0YTavyFTKKo6pA&s" alt="swim"/>
+                            </div>
+                            <div className="col-md-10">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Aliquam nostrum aliquid quas ipsam molestiae quidem cupiditate natus ullam odit nisi!
+                            <div className="e-div">
+                                unlimited wifi
+                            </div>
+                            </div>
                         </div>
+                        </div>
+                        
                     </div>
-                    <div className="col-md-6">
-                        <div className="e-flex">
-
-                        </div>
+                    <div className="col-md-6 e-grid">
+                    <div>
+                    <div className="row">
+                            <div className="col-md-2">
+                            <img src="https://www.freeiconspng.com/uploads/wifi-icon-line-5.png" alt="wifi" />
+                            </div>
+                            <div className="col-md-10">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Aliquam nostrum aliquid quas ipsam molestiae quidem cupiditate natus ullam odit nisi!
+                            <div className="e-div">
+                                unlimited wifi
+                            </div>
+                            </div>
                     </div>
-                    <div className="col-md-6">
-                        <div className="e-flex">
-
-                        </div>
+                    </div>
+                    </div>
+                    <div className="col-md-6 e-grid">
+                    <div>
+                    <div className="row">
+                            <div className="col-md-2">
+                                <img src='https://png.pngtree.com/png-vector/20190811/ourmid/pngtree-power-energy-icon-vector-isolated-png-image_1686577.jpg' alt="energy"/>
+                            </div>
+                            <div className="col-md-10">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            Aliquam nostrum aliquid quas ipsam molestiae quidem cupiditate natus ullam odit nisi!
+                            <div className="e-div">
+                                unlimited wifi
+                            </div>
+                            </div>
+                    </div>
+                    </div>
                     </div>
                     
                     
