467df9944119aa2bf51cf266dc732d7ac015c412
jozzydgr8 <jozzydgreat1@gmail.com>
2024-09-30 02:36:15 -0700

    added 5 min to airport

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -5,13 +5,13 @@ function Welcome() {
   return (
     <section style={{textAlign:'center'}}>
         <div className='  container-fluid'>
-            <h3 className='heading fSection' >
+            <h3 className='heading' >
                 Welcome to Mag's resident
             </h3>
-            <p style={{fontSize:'x-large'}} className='aSection'>
+            <p style={{fontSize:'x-large'}} >
               Mag's residence offers a luxurious serviced apartments located in the heart of
               Osubi, Warri, Delta State. Our apartment is fully equipped and just a 5-minutes 
-              drive from the shopping centers and recreational facilities.
+              drive from Osubi Airport, as well a 3-minutes drive to shopping centers and recreational facilities.
               We provide exclusive and affordable one-bedroom to four-bedroom apartments designed
               to meet the needs of our distinguished clients within Africa and the world at large.
 
