612e224b76ebb2655b7645060dc5caa6b82c7dc8
jozzydgr8 <jozzydgreat1@gmail.com>
2024-10-03 04:48:52 -0700

    added whatdapp url to hadnle request

--- src/Components/IdLayout.jsx
+++ src/Components/IdLayout.jsx
@@ -1,162 +1,139 @@
-import { useEffect, useState } from 'react'
-import Similar from './Similar'
+import { useEffect, useState } from 'react';
+import Similar from './Similar';
 import { UseContextData } from '../ContextFolder/Context/UseContextData';
 import { Link, useParams } from 'react-router-dom';
-import { DatePicker } from 'antd';
-export const IdLayout = ()=>{
+import { Button, DatePicker } from 'antd';
+
+export const IdLayout = () => {
     const [temp, setTemp] = useState(null);
-    const {data} = UseContextData();
-    const {id} = useParams();
+    const { data } = UseContextData();
+    const { id } = useParams();
     const [guest, setGuest] = useState(1);
+    const [checkIn, setCheckIn] = useState(null);
+    const [checkOut, setCheckOut] = useState(null);
+
     useEffect(() => {
         const template = data && data.find(item => item.id === id);
-
         if (template) {
             setTemp(template);
         }
-        console.log(temp)
     }, [data, id]);
-    const url = 
-    temp && temp.fileUrls && temp.fileUrls.map(item=>(
-        item.url
-    ))
-    
 
-    //functions component functions
-    const handleBooking = async (e)=>{
+    const url = temp && temp.fileUrls && temp.fileUrls.map(item => item.url);
+
+    const handleBooking = (e) => {
         e.preventDefault();
-    
-        
-    }
-    
-if(!temp){
-    return
-}
 
-    return(
+        const message = `
+            *Booking Request*\n
+            Check In: ${checkIn}\n
+            Check Out: ${checkOut}\n
+            Guests: ${guest}
+        `.trim().replace(/\n/g, '%0A');
+
+        const whatsappURL = `https://wa.me/08113828486?text=${encodeURIComponent(message)}`;
+
+        window.open(whatsappURL, '_blank');
+    };
+
+    const calculateNights = (checkIn, checkOut) => {
+        if (checkIn && checkOut) {
+            const nights = (new Date(checkOut) - new Date(checkIn)) / (1000 * 3600 * 24);
+            return nights > 0 ? nights : 0;
+        }
+        return 0;
+    };
+
+    if (!temp) {
+        return null; // Return null instead of nothing
+    }
+
+    return (
         <section>
-            <div className='IdBackground' style={{backgroundImage: `url(${url})`}}>
+            <div className='IdBackground' style={{ backgroundImage: `url(${url})` }}>
                 <div className="IdHeader">
                     <h2 className='heading'>{temp.daily}</h2>
-                    <div><span> NAIRA <br/> / PER NIGHT</span></div>
+                    <div><span> NAIRA <br /> / PER NIGHT</span></div>
                 </div>
-                
             </div>
             <div className="container-fluid">
                 <h1 className='heading'>{temp.apartment}</h1>
                 <div className="row">
-                <div className='col-md-8'>
-                <div id="carouselExample" className="carousel slide">
-                    <div className="carousel-inner">
-                        {temp.fileUrls && temp.fileUrls.map((item, index) => (
-                            <div key={index} style={{backgroundImage: `url(${item.url})`}} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                                
+                    <div className='col-md-8'>
+                        <div id="carouselExample" className="carousel slide">
+                            <div className="carousel-inner">
+                                {temp.fileUrls && temp.fileUrls.map((item, index) => (
+                                    <div key={index} style={{ backgroundImage: `url(${item.url})` }} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                    </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
+                        </div>
 
-                    <hr />
-                    <p>
-                        {temp.overview}
-                    </p>
-                    <hr />
-                    <div>
-                        <h1 className='heading'>
-                            Room Services
-                        </h1>
-                        <div className="row">
-                        <div className="col-md-4">
-                                Bedroom Apartment
-                            </div>
-                            <div className="col-md-4">
-                                Unlimited internet
-                            </div>
-                            <div className="col-md-4">
-                                Satelite Tv and Subscriptions
-                            </div>
-                            <div className="col-md-4">
-                                Laundry
-                            </div>
-                            
-                            <div className="col-md-4">
-                                Full time onsite on demand staff
-                            </div>
-                            <div className="col-md-4">
-                                Security
+                        <hr />
+                        <p>{temp.overview}</p>
+                        <hr />
+                        <div>
+                            <h1 className='heading'>Room Services</h1>
+                            <div className="row">
+                                <div className="col-md-4">Bedroom Apartment</div>
+                                <div className="col-md-4">Unlimited internet</div>
+                                <div className="col-md-4">Satellite TV and Subscriptions</div>
+                                <div className="col-md-4">Laundry</div>
+                                <div className="col-md-4">Full-time onsite on-demand staff</div>
+                                <div className="col-md-4">Security</div>
                             </div>
                         </div>
                     </div>
-                    
-
-                </div>
-                <div className="col-md-4">
-                    <div className='IdBooking'>
-                        
-                        <form>
-                            <div className="row">
-                                <div className='col-md-6'>
-                                    <label>check in</label>
-                                    <br/>
-                                        <DatePicker/>
-                                    <br/>
+                    <div className="col-md-4">
+                        <div className='IdBooking'>
+                            <form onSubmit={handleBooking}>
+                                <div className="row">
+                                    <div className='col-md-6'>
+                                        <label>Check In</label>
+                                        <br />
+                                        <DatePicker onChange={setCheckIn} />
+                                        <br />
+                                    </div>
+                                    <div className='col-md-6'>
+                                        <label>Check Out</label>
+                                        <br />
+                                        <DatePicker onChange={setCheckOut} />
+                                        <br />
+                                    </div>
+                                    <div className='col-md-6'>
+                                        <label>Guests</label>
+                                        <br />
+                                        <span>{guest}</span>
+                                        <br />
+                                        <input type='range' min='1' max='8' value={guest}
+                                            onChange={(e) => setGuest(e.target.value)}
+                                        />
+                                        <br />
+                                    </div>
+                                    <div className='col-md-6'>
+                                        <label>Nights</label>
+                                        <br />
+                                        {calculateNights(checkIn, checkOut)} {/* Calculate nights dynamically */}
+                                        <br />
+                                    </div>
                                 </div>
-                                <div className='col-md-6'>
-                                    <label>check out</label>
-                                    <br/>
-                                    <DatePicker/>
-                                    <br/>
-                                </div>
-                                <div className='col-md-6'>
-                                    <label>Guests</label>
-                                    <br/>
-
-                                    <span>{guest}</span>
-                                    <br/>
-                                    
-                                    <input type='range' min='1' max='8' value={guest}
-                                    onChange={(e)=>setGuest(e.target.value)}
-                                    /> <br />
-                                </div>
-                                <div className='col-md-6'>
-                                    <label>Nights</label>
-                                    <br/>
-                                    1
-                                    <br/>
-                                </div>
-
-                            
-                            
-
-                            
-                            
-                            
-                            </div>
-                            
-                            <Link className='regBtn' to="https://wa.link/b8xq56">Book Now</Link>
-                            <Link to="https://wa.link/b8xq56">Reach out to us now</Link>
-
-                            
-                
-                            
-                        </form>
+                                <button type='submit' className='regBtn'>Book Now</button>
+                                <Link to="https://wa.link/b8xq56">Reach out to us now</Link>
+                            </form>
+                        </div>
                     </div>
                 </div>
-                </div>
                 <hr />
-
-                <Similar/>
-
-                
+                <Similar />
             </div>
         </section>
-    )
-}
+    );
+};
--- src/Pages/Rooms.jsx
+++ src/Pages/Rooms.jsx
@@ -3,13 +3,23 @@ import { UseContextData } from "../ContextFolder/Context/UseContextData"
 
 export const Rooms = ()=>{
     const {data} = UseContextData();
+    const shuffleArray = (array) => {
+        const shuffledArray = [...array]; // Create a copy to avoid mutating the original
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+        }
+        return shuffledArray;
+      };
+      
+      let randomArray = data && shuffleArray(data);
     return(
         <section className="room " id="rooms">
             <div className="container-fluid">
                 <h1 className="heading aSection" style={{textAlign:'center'}}>Our Rooms</h1>
                 <div className="row fSection">
                     {
-                        data && data.slice(0,3).map(items =>(
+                        randomArray&& randomArray.slice(0,3).map(items =>(
                             <RoomGrid key={items.id} items={items}/>
                         ))
                     }
